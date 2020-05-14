a6d7d898551ddfd1c8b08ce6f1f60b96c9fc85d5
xuxiaomeng <xuxiaomeng@baozou.com>
2020-05-15 01:02:44 +0800

    feature. update.

--- src/_common/style.js
+++ src/_common/style.js
@@ -2,3 +2,6 @@ import './style.scss';
 import '../_layout/header/style.scss';
 import '../_layout/footer/style.scss';
 import '../index/style.scss';
+import '../livelist/style.scss';
+import '../search/style.scss';
+import '../request/style.scss';
