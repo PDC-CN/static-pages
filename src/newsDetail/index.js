2a8314e80e9c247254852288e3d66d4327039aed
xuxiaomeng <xuxiaomeng@baozou.com>
2020-05-23 12:56:24 +0800

    news detail.

--- src/newsDetail/index.js
+++ src/newsDetail/index.js
@@ -2,6 +2,9 @@ import '../_lib/editor';
 import EditorJS from '@editorjs/editorjs';
 import Header from '@editorjs/header';
 import List from '@editorjs/list';
+import Checklist from '@editorjs/checklist';
+import Table from '@editorjs/table';
+import Image from '@editorjs/image';
 import plugins from '../company/plugins';
 
 const { $, PAGE_DATA } = window;
@@ -21,6 +24,9 @@ function init() {
       //   },
       // },
       List,
+      Checklist,
+      Table,
+      Image,
     },
   });
 
